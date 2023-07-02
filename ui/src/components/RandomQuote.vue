c3644e29c71192fc7ad840d57da3ebcf5416cfc8
Steve Wells <steve.wells@blackbook.ai>
2023-07-02 16:17:12 +1000

    Nicer cards and move button up

--- ui/src/components/RandomQuote.vue
+++ ui/src/components/RandomQuote.vue
@@ -1,19 +1,43 @@
 <template>
-  <div>
-    <v-card class="mb-5">
-      <v-card-title>{{ quote.author }}</v-card-title>
-      <v-card-text>{{ quote.content }}</v-card-text>
-      <v-card-actions>
-        <v-btn outlined color="primary" @click="fetchQuote">New Quote</v-btn>
-      </v-card-actions>
-    </v-card>
-    <div v-for="(oldQuote, index) in oldQuotes" :key="index">
-      <v-card class="mb-2">
-        <v-card-title>{{ oldQuote.author }}</v-card-title>
-        <v-card-text>{{ oldQuote.content }}</v-card-text>
-      </v-card>
-    </div>
-  </div>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" class="text-center mb-5">
+        <v-btn outlined @click="fetchQuote">New Quote</v-btn>
+      </v-col>
+      <v-col cols="4">
+        <v-card
+          class="mx-auto mb-5"
+          max-width="344"
+          outlined
+        >
+          <v-card-text>
+            <div>
+              <div class="text-overline mb-1">{{ quote.author }}</div>
+              <div class="text-h6 mb-1">{{ quote.content }}</div>
+              <div class="text-caption">{{ quote.explanation }}</div>
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row justify="space-between">
+      <v-col v-for="(oldQuote, index) in oldQuotes" :key="index" cols="4">
+        <v-card
+          class="mx-auto mb-4"
+          max-width="344"
+          outlined
+        >
+          <v-card-text>
+            <div>
+              <div class="text-overline mb-1">{{ oldQuote.author }}</div>
+              <div class="text-h6 mb-1">{{ oldQuote.content }}</div>
+              <div class="text-caption">{{ oldQuote.explanation }}</div>
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
