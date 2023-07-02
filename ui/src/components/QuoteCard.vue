4bf16842e47294fc7aea4546477a3f9e65a525ac
Steve Wells <steve.wells@blackbook.ai>
2023-07-02 17:25:10 +1000

    Add tags, UI for explanations imporved

--- ui/src/components/QuoteCard.vue
+++ ui/src/components/QuoteCard.vue
@@ -6,6 +6,13 @@
     <v-card-text class="title mb-6">
       {{ quote.content }}
     </v-card-text>
+    <v-card-subtitle class="my-4">
+      <v-chip-group>
+        <v-chip v-for="(tag, index) in quote.tags" :key="index" small outlined color="grey darken-2">
+          {{ tag }}
+        </v-chip>
+      </v-chip-group>
+    </v-card-subtitle>
     <v-btn class="read-more-btn" text @click="showMore = !showMore" small>
       {{ showMore ? 'Show less' : 'Read more' }}
     </v-btn>
@@ -13,11 +20,25 @@
       {{ quote.explanation }}
     </v-card-text>
     <v-card-text v-else class="caption explanation-text">
-      {{ quote.explanation?.substring(0, 150) + "..." }}
+      {{ quote.explanation?.substring(0, 100) + "..." }}
     </v-card-text>
   </v-card>
 </template>
 
+<style scoped>
+.explanation-text {
+  font-size: 0.8rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.read-more-btn {
+  font-size: 0.6rem;
+  padding: 0.2rem 0.4rem;
+  margin-left: 1rem;
+}
+</style>
 
 <script>
 export default {
